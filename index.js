74a0063f5cab9b050dce5bb6564a844237ee0875
Jacky7 <nedim.akar53411@gmail.com>
2020-09-03 14:02:20 +0200

    socket-project

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 var http = require('http').Server(app);
+// Import Socket.io module
 var io = require('socket.io')(http);
 var ejs = require("ejs")
 
@@ -11,8 +12,11 @@ app.get('/', function(req, res){
   res.render(__dirname + '/index.ejs');
 });
 
+// Our Socket.io module is here
 io.on('connection', function(socket){
+// We take our socket.io message from "chat message"
   socket.on('chat message', function(msg){
+// Send message to server from io.emit
     io.emit('chat message', msg);
   });
 });
